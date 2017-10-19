84c9def1440aaef347f442c6068721752094a4d5
Jeffrey Barros Peña <barrospj@bc.edu>
2017-10-18 20:55:47 -0400

    Add files via upload

---
+++ linked-list-palindrome/LinkedListPalindrome.java
@@ -0,0 +1,155 @@
+import java.util.*;
+
+public class LinkedListPalindrome{
+
+    // CONSTRUCTOR CLASS FOR A LINKED LIST NODE
+    public static class Node{
+        Node next = null;
+        String data;
+
+        public Node(String d){
+            data = d;
+        }
+
+        // ADDS A NODE THAT CONTAINS THE STRING d TO THE END OF THE LINKED LIST
+        public void appendToEnd(String d){
+            Node end = new Node(d);
+            Node n = this;
+            while(n.next != null){
+                n = n.next;
+            }
+            n.next = end;
+        }
+
+        // RETURNS THE LENGTH OF THE ENTIRE PHRASE
+        public int totalStringLen(){
+            Node n = this;
+            int count = 0;
+            while (n.next != null){
+                if (n.data != " "){
+                    count++;
+                }
+                n = n.next;
+            }
+            if (n.data != " "){
+                count++;
+            }
+            return count;
+        }
+
+        // RETURNS THE INDEX RIGHT AT THE HALF WAY POINT OF THE
+        // ENTIRE PHRASE NOT INCLUDING WHITESPACE
+        public int indexAtHalf(){
+            Node n = this;
+            int index = 0;
+            int len = n.totalStringLen();
+            int i = 0;
+            int numOfSpaces = 0;
+            while (index < (len/2+i)){
+                if (n.data != " "){
+                    index++;
+                    n = n.next;
+                } else{
+                    i++;
+                    index++;
+                    n = n.next;
+                    numOfSpaces++;
+                }
+            }
+            if (len % 2 == 1){
+                 index++;
+            }
+            if ((numOfSpaces == 0) || (numOfSpaces == 1)){
+              return index-1;
+            } else{
+              return index;
+            }
+        }
+    }
+
+    public static void main(String args[]){
+        Node head = new Node("n");
+        head.appendToEnd("u");
+        head.appendToEnd("r");
+        head.appendToEnd("s");
+        head.appendToEnd("e");
+        head.appendToEnd("s");
+        head.appendToEnd(" ");
+        head.appendToEnd("r");
+        head.appendToEnd("u");
+        head.appendToEnd("n");
+
+
+        printList(head);
+        boolean result = isPalindrome(head);
+        if (result == true){
+            System.out.println("Given linked list is a palindrome!");
+        } else{
+            System.out.println("Given linked list is not a palindrome.");
+        }
+    }
+
+    // DETERMINES IF A LINKED LIST IS A PALINDROME
+    public static boolean isPalindrome(Node head){
+        Node n = head;
+        Stack<String> stack = new Stack<String>();
+
+        int count = 0;
+        int len = head.totalStringLen();
+        int index = head.indexAtHalf();
+        String s = "";
+            while (head.next != null){
+                while (count < index){
+                    count++;
+                    if (head.data != " "){
+                        stack.push(head.data);
+                    }
+                    head = head.next;
+
+                    // CHECK IF LENGTH OF THE LINKED LIST IS ODD OR EVEN
+                    if ((count == index) && (len%2==1)){
+                        head = head.next;
+                    } else if ((count==index) && (len%2==0)){
+                        stack.push(head.data);
+                        head = head.next;
+                    }
+                }
+
+                if (head.data == " "){
+                    head = head.next;
+                    if (head.next == null){
+                      break;
+                    }
+                }
+                s = stack.pop();
+                if (s != head.data){
+                    return false;
+                } else{
+                    head = head.next;
+                    if (stack.isEmpty()){
+                      return true;
+                    }
+                }
+            }
+            if (!stack.isEmpty()){
+              s = stack.pop();
+            }
+            if(head==null){
+              return true;
+            }
+            // CHECK THE LINKED LIST'S FINAL STRING ELEMENT
+            if (s != head.data){
+                return false;
+            }
+            return true;
+  }
+
+    // PRINTS A LINKED LIST
+    public static void printList(Node head){
+        while (head != null){
+            System.out.print(head.data + " -> ");
+            head = head.next;
+        }
+        System.out.println(".");
+    }
+}
---
+++ linked-list-palindrome/README.md
@@ -0,0 +1,6 @@
+# LinkedListPalindrome.java
+An algorithm that determines whether a given linked list is a palindrome (a word, phrase, or sequence that reads the same backward as forward) or not. Time complexity of the method of O(n).
+
+A stack is used to record the first half of the word/phrase in reversed order to then compare the value that is popped off the stack with the second half of the word/phrase.
+
+Change the given linked list in the main method by creating a node with the first letter of the word/phrase then appending subsequent letters, numbers, or spaces in a String type.
